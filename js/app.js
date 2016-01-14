8c6a76d0e6df0bb931cad2d10dae83de41346934
Marcus Michaels <hello@modestindustries.co>
2016-01-14 13:05:02 +0000

    Added contact page and contact form styling. Need to add PHP form submit functionality

--- js/app.js
+++ js/app.js
@@ -1,46 +1,17 @@
-// Google API Key – AIzaSyCKfjcLEpUCmMzLag352nl5kr8F5HQg-4w
-// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCKfjcLEpUCmMzLag352nl5kr8F5HQg-4w&callback=initMap" async defer></script>
+var menuOpen = false;
+var menu_drop = document.getElementById('menu-drop');
+var menu_x = document.getElementById('menu-close-x');
 
-// var map;
-// function initMap() {
-
-//   var image = 'marker.png';
-//   var beachMarker = new google.maps.Marker({
-//       position: {lat: 51.5072, lng: 0.1275},
-//       map: map,
-//       icon: image
-//     });
-
-//   var customMapType = new google.maps.StyledMapType([
-//       {
-//         stylers: [
-//           {saturation:-100},
-//           {visibility: 'simplified'}
-//         ]
-//       },
-//       {
-//         elementType: 'labels',
-//         stylers: [{visibility: 'off'}]
-//       },
-//       {
-//         featureType: 'water',
-//         stylers: [{color: '#fcfcfc'}]
-//       }
-//     ], {
-//       name: 'Custom Style'
-//   });
-//   var customMapTypeId = 'custom_style';
-
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 13,
-//     center: {lat: 51.5072, lng: 0.1275},  // London.
-//     disableDefaultUI: false,
-//     mapTypeControlOptions: {
-//       mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
-//     }
-//   });
-
-//   map.mapTypes.set(customMapTypeId, customMapType);
-//   map.setMapTypeId(customMapTypeId);
-// }
-
+function toggleMenu(){ 
+  if (!menuOpen) {
+    menu_drop.style.display="block";
+    menu_x.style.display="block";
+    menuOpen = true;
+    // console.log("Menu opened");
+  } else {
+    menu_drop.style.display="none";
+    menu_x.style.display="none";
+    // console.log("Menu closed");
+    menuOpen = false;
+  }
+}
