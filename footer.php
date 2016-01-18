3fd466e0b31e50111006d1c043b09849f81d6794
Marcus Michaels <hello@modestindustries.co>
2016-01-18 15:30:29 +0000

    added toggle animation for mobile menu

--- footer.php
+++ footer.php
@@ -15,13 +15,14 @@
     <script src="js/CSSPlugin.min.js"></script>
     <script src="js/EasePack.min.js"></script>
     <script src="js/TweenLite.min.js"></script>
+    <script src="js/main.js"></script>
 
     <script type="text/javascript">
       (function(e,t){"use strict";var n=0,r=175,i=15,s=document.getElementsByTagName("a"),o;for(var u=0;u<s.length;u++){o=s[u].attributes.href===t?null:s[u].attributes.href.nodeValue.toString();if(o!==null&&o.length>1&&o.indexOf("#")!=-1){s[u].onclick=function(){var n,s=this.attributes.href.nodeValue.toString(),o=s.substr(0,s.indexOf("#")),u=s.substr(s.indexOf("#")+1);if(n=document.getElementById(u)){var l=(r-r%i)/i,c=f(),h=(a(n)-c)/l;if(e.history&&typeof e.history.pushState=="function")e.history.pushState({},t,o+"#"+u);for(var p=1;p<=l;p++){(function(){var t=h*p;setTimeout(function(){e.scrollTo(0,t+c)},i*p)})()}return false}}}}var a=function(e){var r=n*-1;while(e.offsetParent!=t&&e.offsetParent!=null){r+=e.offsetTop+(e.clientTop!=null?e.clientTop:0);e=e.offsetParent}return r};var f=function(){return e.pageYOffset!==t?e.pageYOffset:document.documentElement.scrollTop!==t?document.documentElement.scrollTop:document.body.scrollTop}})(window);
     </script>
 
     <?php echo $custom_scripts; ?>
-    <script src="js/main.js"></script>
+
   </body>
 </html>
 
--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
-var menuOpen = false;
+// ANIMATED MOBILE TOGGLE MENU
+var menuOpen = false,
+    animating = false;
 var menu_drop = document.getElementById('menu-drop');
 var menu_x = document.getElementById('menu-close-x');
 
 function toggleMenu(){ 
-  if (!menuOpen) {
+  if (!menuOpen && !animating) {
+    animating = true;
     menu_drop.style.display="block";
     menu_x.style.display="block";
-    menuOpen = true;
-    // console.log("Menu opened");
-  } else {
-    menu_drop.style.display="none";
-    menu_x.style.display="none";
-    // console.log("Menu closed");
-    menuOpen = false;
+    TweenLite.fromTo(menu_drop,0.4,{height:0,marginBottom:0},{height:207,marginBottom:36, onComplete:function(){
+      animating = false;
+      menuOpen = true;
+    }});
+  } else if (menuOpen && !animating) {
+    animating = true;
+    TweenLite.to(menu_drop,0.4,{height:0,marginBottom:0, onComplete:function(){
+      menu_drop.style.display="none";
+      menu_x.style.display="none";
+      menuOpen = false;
+      animating = false;
+    }});
   }
 }
+
