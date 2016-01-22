0c07979ca059910504dde2440ef321c213211236
Marcus Michaels <hello@modestindustries.co>
2016-01-22 18:30:49 +0000

    Added box-sizing and removed 100% buttons in place of letting the margin:auto and left:0;right:0; putting things in place so if box-sizing ever fails the border doesn't get cut off... don't actually need the CSS3 box-sizing actually anymore.

--- services.php
+++ services.php
@@ -16,23 +16,37 @@ $custom_scripts = "";
 
 include 'header.php'; ?>
 
+  <div class="section text">
+    <h2 class="col1">A few things to get you going</h2>
+    <p class="col2">The services we provide are bespoke to the client as there's no one-size-fits-all solution. Find out how we can help you by getting in touch for a friendly no-obligation chat with us.</p>
+    <p class="col2">For every problem there are countless solutions, so below are some of the services we offer to give you an idea of how we can help and the areas in which we excel.</p>
+  </div>
+
   <div class="section service">
 
-    <h2 class="col1">Brand Identity</h2>
+    <h2 class="col1">Company Kickstarter</h2>
+    <img src="img/placeholder-phone.jpg" class="col2 col-image">
+    <p class="col2 col-text">You have a name, an idea, and you want to make it happen. What's next? We'll work with you to develop how your brand's identity, personality and online presence, putting you centre stage to your audience. For those offline interactions we'll kit you out with memorable business cards and materials relevant to your brand.</p>
+    <div class="clearfix"></div>
+    <a class="cta-btn" href="contact">Kickstart your Company</a>
+
+    <h2 class="col1">Talking the talk</h2>
     <img src="img/placeholder-phone.jpg" class="col2 col-image">
     <p class="col2 col-text"> Bacon ipsum dolor amet pork belly spare ribs sirloin pancetta short ribs. Capicola boudin pork chop meatball filet mignon landjaeger meatloaf turkey beef ribs ball tip kielbasa cow ground round doner. Shankle tenderloin pork belly meatloaf saeatball filet mignon landjaeger meatloaf turkey beef ribs ball tip kielbasa cow ground round doner.</p>
+    <div class="clearfix"></div>
+    <a class="cta-btn" href="contact">Kickstart your Company</a>
 
     <h2 class="col1">Brand Identity</h2>
     <img src="img/placeholder-phone.jpg" class="col2 col-image">
     <p class="col2 col-text"> Bacon ipsum dolor amet pork belly spare ribs sirloin pancetta short ribs. Capicola boudin pork chop meatball filet mignon landjaeger meatloaf turkey beef ribs ball tip kielbasa cow ground round doner. Shankle tenderloin pork belly meatloaf saeatball filet mignon landjaeger meatloaf turkey beef ribs ball tip kielbasa cow ground round doner.</p>
+    <div class="clearfix"></div>
+    <a class="cta-btn" href="contact">Kickstart your Company</a>
 
     <h2 class="col1">Brand Identity</h2>
     <img src="img/placeholder-phone.jpg" class="col2 col-image">
     <p class="col2 col-text"> Bacon ipsum dolor amet pork belly spare ribs sirloin pancetta short ribs. Capicola boudin pork chop meatball filet mignon landjaeger meatloaf turkey beef ribs ball tip kielbasa cow ground round doner. Shankle tenderloin pork belly meatloaf saeatball filet mignon landjaeger meatloaf turkey beef ribs ball tip kielbasa cow ground round doner.</p>
-
-    <h2 class="col1">Brand Identity</h2>
-    <img src="img/placeholder-phone.jpg" class="col2 col-image">
-    <p class="col2 col-text"> Bacon ipsum dolor amet pork belly spare ribs sirloin pancetta short ribs. Capicola boudin pork chop meatball filet mignon landjaeger meatloaf turkey beef ribs ball tip kielbasa cow ground round doner. Shankle tenderloin pork belly meatloaf saeatball filet mignon landjaeger meatloaf turkey beef ribs ball tip kielbasa cow ground round doner.</p>
+    <div class="clearfix"></div>
+    <a class="cta-btn" href="contact">Kickstart your Company</a>
 
   </div>
 
