07d4765cd3622a436d6c6c8a682c4c205e83e8cc
Marcus Michaels <hello@modestindustries.co>
2016-10-20 13:45:04 +0100

    Optimised images

--- products.php
+++ products.php
@@ -23,13 +23,6 @@ include 'header.php'; ?>
   </div>
 
   <div class="section product">
-    <div class="product-box col2">
-      <img src="img/png/sweat-roulette.png" class="product-image">
-      <h2>Sweat Roulette</h2>
-      <p>Sweat Roulette is a cross-platform web-app made to help you get fit. It's easy to get started, there's only one rule – do the exercise for as long as it says on the screen.</p>
-      <p>Each exercise will last for 20-40 seconds before changing. Keep going as hard as you can for as long as you can. One point per exercise so once you're done, tweet your score. #humblebrag.</p>
-      <a href="http://sweatroulette.com" target="_blank" class="main-button">Play Sweat Roulette</a>
-    </div>
  
     <div class="product-box col2">
       <a href="http://deadwallet.com" target="_blank">
@@ -40,6 +33,17 @@ include 'header.php'; ?>
       <p>This project that will continue to be developed and evolve over time. If you'd like to collaborate or want to share some constructive feedback, get in touch let's talk about it.</p>
 
       <a href="http://deadwallet.com" target="_blank" class="main-button">Visit Dead Wallet</a>
+    </div>
+
+    <div class="product-box col2">
+      <a href="http://sweatroulette.com" target="_blank">
+        <img src="img/png/sweat-roulette.png" class="product-image">
+      </a>
+      <h2>Sweat Roulette</h2>
+      <p>Sweat Roulette is a cross-platform prototype made to help you get fit. It's easy to get started, there's only one rule – do the exercise for as long as it says on the screen.</p>
+      <p>Each exercise will last for 20-40 seconds before changing. Keep going as hard as you can for as long as you can. One point per exercise so once you're done, tweet your score. #humblebrag.</p>
+
+      <a href="http://sweatroulette.com" target="_blank" class="main-button">Play Sweat Roulette</a>
     </div>
 
   </div>
