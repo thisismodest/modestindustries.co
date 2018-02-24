03bb3253dc49c76c5fd30e97de0ef4514296f180
Marcus Michaels <hello@modestindustries.co>
2018-02-24 11:49:30 +0000

    Replaces Sweat Roulette with PLottd

--- products.php
+++ products.php
@@ -23,19 +23,30 @@ include 'header.php'; ?>
   </div>
 
   <div class="section product">
- 
+  
     <div class="product-box col2">
-      <a href="http://deadwallet.com" target="_blank">
+      <a href="https://plottd.com" title="Plottd" target="_blank">
+        <img src="img/png/plottd-by-modest-industries.png" class="product-image">
+      </a>
+      <h2>Plottd.com</h2>
+      <p>Plottd allows you to easily add interactive hotspots to your images and embed them on your website or blog. Perfect for content publishers and independent businesses.</p>
+      <p>Unlike other companies providing a similar service, Plottd aims to be as accessible as possible, which is why it'll be free and not require sign up to do basic image plotting.</p>
+
+      <a href="https://plottd.com" title="Plottd" target="_blank" class="main-button">Visit Plottd</a>
+    </div>
+
+    <div class="product-box col2">
+      <a href="http://deadwallet.com" title="Dead Wallet" target="_blank">
         <img src="img/png/dead-wallet-by-modest-industries.png" class="product-image">
       </a>
       <h2>DeadWallet.com</h2>
       <p>Stuff you didn't know you wanted, beautifully curated. Dead Wallet scours the web for the kind of stuff that'll kill your wallet. That's why we called it Dead Wallet. Clever, huh?</p>
-      <p>This project that will continue to be developed and evolve over time. If you'd like to collaborate or want to share some constructive feedback, get in touch let's talk about it.</p>
+      <p>Dead Wallet will continue to be developed and evolve over time. If you'd like to collaborate or want to share some constructive feedback, get in touch let's talk about it.</p>
 
-      <a href="http://deadwallet.com" target="_blank" class="main-button">Visit Dead Wallet</a>
+      <a href="https://deadwallet.com" title="Dead Wallet" target="_blank" class="main-button">Visit Dead Wallet</a>
     </div>
 
-    <div class="product-box col2">
+    <!-- <div class="product-box col2">
       <a href="http://sweatroulette.com" target="_blank">
         <img src="img/png/sweat-roulette.png" class="product-image">
       </a>
@@ -44,7 +55,7 @@ include 'header.php'; ?>
       <p>Each exercise will last for 20-40 seconds before changing. Keep going as hard as you can for as long as you can. One point per exercise so once you're done, tweet your score. #humblebrag.</p>
 
       <a href="http://sweatroulette.com" target="_blank" class="main-button">Play Sweat Roulette</a>
-    </div>
+    </div> -->
 
   </div>
 
